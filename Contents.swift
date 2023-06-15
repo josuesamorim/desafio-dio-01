278f19cb72fcc7869b96d70cb71ae072ffa23b66
Josue Amorim <josuedesouzaamorim@gmail.com>
2023-06-15 09:27:52 -0300

    Ok

--- Contents.swift
+++ Contents.swift
@@ -1,6 +1,12 @@
 import Cocoa
 
+
 let firstName = "Steve"
 var lastName: String? = "Jobs"
 
-print(firstName, lastName!)
+
+print(firstName, lastName ?? "Wozniak")
+
+if let name = lastName {
+    print(firstName, name)
+}
